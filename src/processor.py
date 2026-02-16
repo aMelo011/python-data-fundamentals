56529781c8ce62a8c187afc08cd45bcea3ea0abf
aMelo011 <icaromelo011@gmail.com>
2026-02-16 11:12:14 +0000

    feat: added a script to fetch data from an API + created requirements.txt

--- src/processor.py
+++ src/processor.py
@@ -6,8 +6,9 @@ with open("data/data.csv", "r") as file:
         parts = clean_line.split(",")
         temperatures.append(float(parts[1]))
 
-print(temperatures)
-
 if len(temperatures) > 0:
     print(sum(temperatures) / len(temperatures))
+    print(temperatures)
 else: print("Error on data file")
+
+
